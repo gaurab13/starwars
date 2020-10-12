de604a7b0e443d8211caf97ad22e225edfb3472b
Gaurav KC <13gaurab@gmail.com>
2020-10-12 18:48:16 +0545

    Setup base layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Navbar from './components/Navbar';
+import People from './components/People';
+import Planets from './components/Planets';
 
 function App() {
+  const [page, setPage] = useState('planets');
   return (
     <div className="App">
-      React
+      <h1>Star Wars</h1>
+      <Navbar setPage={setPage} />
+      <div className="content">
+        {page === 'planets' ? <Planets /> : <People />}
+      </div>
     </div>
   );
 }
